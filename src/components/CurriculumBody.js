a8c2b81c27aaf1f7230ef09ab6425c7f861c2151
JuluanQ <48562224+JuluanQ@users.noreply.github.com>
2022-07-12 20:18:15 +0200

    CurriculumBody advancements

--- src/components/CurriculumBody.js
+++ src/components/CurriculumBody.js
@@ -22,16 +22,41 @@ const CurriculumBody = () => {
                     <h3>Age : <span>23</span></h3>
                     <h3>Gender : <span>Male</span></h3>
                     <h3>Location : <span>Quimper, Finistère, Bretagne, France</span></h3>
-                    <h3> : <span></span></h3>
+                    <h3>Hobbies : <span>Video games, Japanese culture, Coding</span></h3>
+                    <h3>Music styles : <span>Metal, Rock, EDM</span></h3>
                 </div>
             </div>
             <div className="mainCardCurriculum">
-
+                <div className="a item RightItem">
+                    <h3 className='aaa'>2018</h3>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+                <div className='lane laneA'></div>
+                <div className="b item LeftItem">
+                    <h3 className='bbb'>2019</h3>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+                <div className='lane laneB'></div>
+                <div className="c item RightItem">
+                    <h3 className='ccc'>2020</h3>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+                <div className='lane laneC'></div>
+                <div className="d item LeftItem">
+                    <h3 className='ddd'>2021</h3>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+                <div className='lane laneD'></div>
+                <div className="e item RightItem">
+                    <h3 className='eee'>2022</h3>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+                <div className='lane laneE'></div>
             </div>
             <div className="rightPanel">
 
             </div>
-        </motion.div>
+        </motion.div >
     );
 };
 
